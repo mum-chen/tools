515438a5c9cd10ca377b4b2c40b74c5a33883c60
mum-chen <542177535@qq.com>
2016-11-27 21:39:48 +0800

    add lua templet & compelete vimrc

---
+++ document/templet.lua
@@ -0,0 +1,25 @@
+--[[
+@auth   mum-chen
+@desc   
+@date   
+--]]
+--======== include and declare constant =======================================
+-- load config
+-- load tool
+-- load model
+--======== import function and declare constant ================================
+
+--======== public function =====================================================
+
+---------- module delimiter ----------------------------------------------------
+
+--[[
+@func:	setport
+--------------------
+@desc:	set mac-table by mac and port
+
+@param:	
+ mac:	only get the last 48bit
+ port:	only get the last 2bit
+@return:	
+--]]
